f1b7c728e3ff97f9c154af921301a59fba9688fe
Iridium <iridiumlegeartis@gmail.com>
2020-08-24 07:14:01 +0300

    Initial Commit

---
+++ HW2.2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.2.1
+//
+//  Created by Игорь Зигура on 24.08.2020.
+//  Copyright © 2020 Zigura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
