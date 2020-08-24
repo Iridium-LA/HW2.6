d0a1c92cbf4542bf7e2567994d84a38841d5d40b
Iridium <iridiumlegeartis@gmail.com>
2020-08-24 08:37:20 +0300

    Release

--- HW2.2.1/ViewController.swift
+++ HW2.2.1/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorView: UIView!
+    
+    func sliderMove() {
+        colorView.layer.backgroundColor = CGColor(srgbRed: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        redLabel.text = String(round(redSlider.value * 100) / 100)
+        greenLabel.text = String(round(greenSlider.value * 100) / 100)
+        blueLabel.text = String(round(blueSlider.value * 100) / 100)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func redSliderMove() {sliderMove()}
+    @IBAction func greenSliderMove() {sliderMove()}
+    @IBAction func blueSliderMove() {sliderMove()}
+    
 }
 
